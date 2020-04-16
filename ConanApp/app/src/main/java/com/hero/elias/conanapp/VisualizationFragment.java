445ca2b68e974574257d33457324382d31bbb09f
Alfred Runn <affe21@hotmail.com>
2020-04-16 03:12:15 +0200

    Initial Implementation of Visualization

--- ConanApp/app/src/main/java/com/hero/elias/conanapp/VisualizationFragment.java
+++ ConanApp/app/src/main/java/com/hero/elias/conanapp/VisualizationFragment.java
@@ -1,14 +1,21 @@
 package com.hero.elias.conanapp;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentViewHolder;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class VisualizationFragment extends Fragment {
+    
+    ImageView imageView;
     
     public VisualizationFragment() {
     }
@@ -26,5 +33,12 @@ public class VisualizationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_visualization, container, false);
+    }
+    
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        this.imageView = (ImageView) view.findViewById(R.id.visualization_imageview);
+        
+        super.onViewCreated(view, savedInstanceState);
     }
 }
