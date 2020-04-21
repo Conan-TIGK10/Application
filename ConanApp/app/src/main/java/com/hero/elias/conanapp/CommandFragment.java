1ade5f8f09418dde825fd66cf525ccffc4269a1e
Elias Lindh <viduell@gmail.com>
2020-04-21 18:16:26 +0200

    implemented a joystick that tells the value of where you are holding.

--- ConanApp/app/src/main/java/com/hero/elias/conanapp/CommandFragment.java
+++ ConanApp/app/src/main/java/com/hero/elias/conanapp/CommandFragment.java
@@ -1,5 +1,7 @@
 package com.hero.elias.conanapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,14 +9,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class CommandFragment extends Fragment implements BluetoothHandler.BluetoothCallback, View.OnClickListener {
     Button sendButton;
     TextView receiveTextView;
+    Bitmap ball;
+    JoyStickClass joystick;
+    RelativeLayout layout_joystick;
+    ImageView image_joystick, image_border;
+    TextView textView_x, textView_y, textView_angle, textView_distance, textView_direction;
     
     public CommandFragment() {
         BluetoothHandler.getInstance().addCallback(this);
@@ -37,13 +47,12 @@ public class CommandFragment extends Fragment implements BluetoothHandler.Blueto
     }
     
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
+    public void onCreate(Bundle savedInstanceState) { super.onCreate(savedInstanceState); }
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_command, container, false);
+
     }
     
     @Override
@@ -53,7 +62,71 @@ public class CommandFragment extends Fragment implements BluetoothHandler.Blueto
         
         this.receiveTextView = view.findViewById(R.id.receive_text);
         super.onViewCreated(view, savedInstanceState);
+
+        ball = BitmapFactory.decodeResource(getResources(), R.drawable.purple_ball);
+
+        textView_x = view.findViewById(R.id.x_value);
+        textView_y = view.findViewById(R.id.y_value);
+        textView_angle = view.findViewById(R.id.angle);
+        textView_distance = view.findViewById(R.id.distance);
+        textView_direction = view.findViewById(R.id.direction);
+
+        layout_joystick = view.findViewById(R.id.JoystickBackground);
+
+        // send the image that pops up as the onclicklistener
+        joystick = new JoyStickClass(getActivity().getApplicationContext(), layout_joystick, R.drawable.image_button);
+
+        joystick.setStickSize(150, 150);
+        joystick.setLayoutSize(500, 500);
+        joystick.setLayoutAlpha(150);
+        joystick.setStickAlpha(100);
+        joystick.setOffset(90);
+        joystick.setMinimumDistance(50);
+
+        layout_joystick.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                joystick.drawStick(arg1);
+                if(arg1.getAction() == MotionEvent.ACTION_DOWN
+                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+                    textView_x.setText("X : " + joystick.getX());
+                    textView_y.setText("Y : " + joystick.getY());
+                    textView_angle.setText("Angle : " + joystick.getAngle());
+                    textView_distance.setText("Distance : " + joystick.getDistance());
+
+                    int direction = joystick.get8Direction();
+                    if(direction == JoyStickClass.STICK_UP) {
+                        textView_direction.setText("Direction : Up");
+                    } else if(direction == JoyStickClass.STICK_UPRIGHT) {
+                        textView_direction.setText("Direction : Up Right");
+                    } else if(direction == JoyStickClass.STICK_RIGHT) {
+                        textView_direction.setText("Direction : Right");
+                    } else if(direction == JoyStickClass.STICK_DOWNRIGHT) {
+                        textView_direction.setText("Direction : Down Right");
+                    } else if(direction == JoyStickClass.STICK_DOWN) {
+                        textView_direction.setText("Direction : Down");
+                    } else if(direction == JoyStickClass.STICK_DOWNLEFT) {
+                        textView_direction.setText("Direction : Down Left");
+                    } else if(direction == JoyStickClass.STICK_LEFT) {
+                        textView_direction.setText("Direction : Left");
+                    } else if(direction == JoyStickClass.STICK_UPLEFT) {
+                        textView_direction.setText("Direction : Up Left");
+                    } else if(direction == JoyStickClass.STICK_NONE) {
+                        textView_direction.setText("Direction : Center");
+                    }
+                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
+                    textView_x.setText("X :");
+                    textView_y.setText("Y :");
+                    textView_angle.setText("Angle :");
+                    textView_distance.setText("Distance :");
+                    textView_direction.setText("Direction :");
+                }
+                return true;
+            }
+        });
+
     }
+
+
     
     @Override
     public void onDestroy() {
